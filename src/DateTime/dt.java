834252eef840b95b8c89016b953040f8f1715e13
Qoreas <buyfrtscbhbivpz@gmail.com>
2022-11-05 21:20:48 +0300

    classwork + homework

---
+++ src/DateTime/dt.java
@@ -0,0 +1,12 @@
+package DateTime;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class dt {
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss MM/dd/yyyy");
+        LocalDateTime ldt = LocalDateTime.now();
+        System.out.println(ldt.format(formatter));
+    }
+}
---
+++ src/HomeWork/L2/T2.java
@@ -0,0 +1,9 @@
+package HomeWork.L2;
+
+public class T2 {
+    public static void main(String[] args) {
+        double x = 1.1, y = 0.5;
+        double task = (x * (8.3 - y) / x - y) * (x + y);
+        System.out.println(task);
+    }
+}
