b1c0280096a84fa36da92728c8cbe776f70237b8
Qoreas <buyfrtscbhbivpz@gmail.com>
2022-11-13 18:51:02 +0300

    homework refactor

--- src/HomeWork/L4/T2_CountryXYZ.java
+++ src/HomeWork/L4/T2_CountryXYZ.java
@@ -3,9 +3,11 @@ package HomeWork.L4;
 public class T2_CountryXYZ {
     public static void main(String[] args) {
         int civil = 10_000_000;
-        int burn = 140_000; // per 10m
-        int death = 80_000; // per 10m
+        int percent = 1000;
         int years = 10;
+
+        int burn = civil / percent * 14;
+        int death = civil / percent * 8;
 
         for (int i = 0; i <= years; i++) {
             civil += burn - death;
