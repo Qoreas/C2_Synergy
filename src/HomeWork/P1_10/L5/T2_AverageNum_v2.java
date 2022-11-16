545921e86d86d985d8e17232de06b65a3120c873
Qoreas <buyfrtscbhbivpz@gmail.com>
2022-11-16 23:34:44 +0300

    homework

--- src/HomeWork/P1_10/L5/T2_AverageNum_v2.java
+++ src/HomeWork/P1_10/L5/T2_AverageNum_v2.java
@@ -7,12 +7,10 @@ public class T2_AverageNum_v2 {
         Scanner scan = new Scanner(System.in);
 
         System.out.print("Input array size: ");
-
         int size = scan.nextInt();
         int[] array = new int[size];
 
         System.out.println("Input numbers: ");
-
         double counter = 0;
         for (int i = 0; i < array.length; i++) {
             array[i] = scan.nextInt();
---
+++ src/HomeWork/P1_10/L5/T4_Matrix.java
@@ -0,0 +1,15 @@
+package HomeWork.P1_10.L5;
+
+public class T4_Matrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{2, 3}, {4, 5}, {7, 8}, {9, 0}};
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
