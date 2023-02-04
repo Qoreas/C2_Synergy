3abd03f586b682c80b68b2473bf9441421347144
Qoreas <buyfrtscbhbivpz@gmail.com>
2023-02-04 20:45:29 +0300

    classwork

---
+++ src/semester_2/classwork/P1_9/CW1/Division.java
@@ -0,0 +1,34 @@
+package semester_2.classwork.P1_9.CW1;
+
+import java.util.Scanner;
+
+public class Division {
+    Scanner sc = new Scanner(System.in);
+    private double num1;
+    private double num2;
+
+    private void setNum1() {
+        this.num1 = sc.nextInt();
+    }
+
+    private void setNum2() {
+        this.num2 = sc.nextInt();
+    }
+
+    private double division() throws ArithmeticException {
+        setNum1();
+        setNum2();
+        if (num2 == 0) throw new ArithmeticException();
+        return num1 / num2;
+    }
+
+    public void divided() {
+        try {
+            double result = division();
+            System.out.println("a / b = " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Try again");
+            divided();
+        }
+    }
+}
---
+++ src/semester_2/classwork/P1_9/CW1/T1.java
@@ -0,0 +1,8 @@
+package semester_2.classwork.P1_9.CW1;
+
+public class T1 {
+    public static void main(String[] args) {
+        Division d = new Division();
+        d.divided();
+    }
+}
