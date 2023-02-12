72fb639fbf630147520d1ccede0520162e4254e0
Qoreas <buyfrtscbhbivpz@gmail.com>
2023-02-12 20:57:08 +0300

    homework attempt

--- src/semester_2/homework/P1_9/HW2/LoginValidatorException.java
+++
@@ -1,6 +0,0 @@
-package semester_2.homework.P1_9.HW2;
-
-public class LoginValidatorException extends Exception {
-
-}
-
--- src/semester_2/homework/P1_9/HW2/test.java
+++
@@ -1,7 +0,0 @@
-package semester_2.homework.P1_9.HW2;
-
-public class test {
-    public static void main(String[] args) {
-        check.validateLogin("Hell_o");
-    }
-}
