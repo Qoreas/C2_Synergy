8adb66d6f5ae3a4db7fc0edfd12622f5096a86fa
Qoreas <buyfrtscbhbivpz@gmail.com>
2023-04-09 13:23:59 +0300

    semester 3: 1 lesson - generics

---
+++ src/semester_3/classwork/W1_generics/T1_createGeneric.java
@@ -0,0 +1,38 @@
+package semester_3.classwork.W1_generics;
+
+public class T1_createGeneric {
+    public static void main(String[] args) {
+        Box<Paper> boxForPaper = new Box<>();
+        Box<Plastic> boxForPlastic = new Box<>();
+    }
+}
+
+class Box<T extends T6_garbage> {
+    private T item;
+
+    public void putItem(T item) {
+        this.item = item;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public double getItemWeight() {
+        return item == null ? 0 : item.getWeight();
+    }
+}
+
+class Paper extends T6_garbage {
+    @Override
+    public double getWeight() {
+        return 0.01;
+    }
+}
+
+class Plastic extends T6_garbage {
+    @Override
+    public double getWeight() {
+        return 0.3;
+    }
+}
---
+++ src/semester_3/classwork/W1_generics/T6_garbage.java
@@ -0,0 +1,5 @@
+package semester_3.classwork.W1_generics;
+
+abstract public class T6_garbage {
+    public abstract double getWeight();
+}
