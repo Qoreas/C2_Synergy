1d8aac0e6c270c846885ee716d9cecab200b4beb
Qoreas <buyfrtscbhbivpz@gmail.com>
2023-04-09 18:30:40 +0300

    homework 1 - generics

---
+++ src/semester_3/homework/W1/Main.java
@@ -0,0 +1,11 @@
+package semester_3.homework.W1;
+
+public class Main {
+    public static void main(String[] args) {
+        Sword<Copper> sword = new Sword<>(new Copper());
+        System.out.println(sword.checkEndurance());
+
+        Sword<Steel> sword2 = new Sword<>(new Steel());
+        System.out.println(sword2.checkEndurance());
+    }
+}
